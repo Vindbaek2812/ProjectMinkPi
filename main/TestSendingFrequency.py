98f1039da04bcfd2d88f42532ef7e5fac4e936f2
Christian Vindbæk <christianvf99@gmail.com>
2020-09-30 14:58:22 +0200

    Cleaning the code

--- main/TestSendingFrequency.py
+++ main/TestSendingFrequency.py
@@ -16,6 +16,7 @@ MechanicalMotTime = 10
 MotRunTimeHour = 10
 MotRunTimeMin = 10
 alarms=""
+RabbitMessage = ""
 
 # Set the delay time
 delaytime = 1
@@ -24,9 +25,7 @@ delaytime = 1
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-# Define the JSON message to send to IoT Hub.
-#MSG_TXT = '{{"motorTemp": {motorTemp},"hydraulicTemp": {hydraulicTemp},"fuelLevel":{fuelLevel},"hydraulicPressure":{hydraulicPressure}, "alarms":{alarms}, "nowTime": {nowTime}}}'
-Rabbitmessage = ""
+
 # Establish the exchange
 channel.exchange_declare(exchange='sensor_exchange', exchange_type='topic')
 
@@ -34,33 +33,31 @@ channel.exchange_declare(exchange='sensor_exchange', exchange_type='topic')
 can_interface = 'can0'
 bus = can.interface.Bus(can_interface, bustype='socketcan')
 
-#assembles the message with sensorDATA
+#assembles the message with sensorDATA from each column
 def makeString(stringPart):
-    global Rabbitmessage
-    if(Rabbitmessage==""):
-        Rabbitmessage = Rabbitmessage + stringPart
+    global RabbitMessage
+    if(RabbitMessage==""):
+        RabbitMessage = RabbitMessage + stringPart
     else:
-        Rabbitmessage = Rabbitmessage + ',' + stringPart
+        RabbitMessage = RabbitMessage + ',' + stringPart
     
 
 
 def SendString():
-    global Rabbitmessage
-    print(len(Rabbitmessage)>0)
-    if(len(Rabbitmessage) > 0):
+    global RabbitMessage
+    if(len(RabbitMessage) > 0):
         nowdatetime = datetime.now()
         nowTime = str(nowdatetime.strftime('%d/%m/%y - %H:%M:%S'))
-        nowTime = '"' + nowTime + '"'
-        Rabbitmessage=('{' + Rabbitmessage + ',' + nowTime + '}')
+        RabbitMessage=('{' + RabbitMessage + ',' + '"' + nowTime + '"' + '}')
 
         #This line is sending the message to RabbitMQ
-        #channel.basic_publish(exchange='sensor_exchange',routing_key='sensorData',body=Rabbitmessage)
-        print(Rabbitmessage)
-        Rabbitmessage=""
+        #channel.basic_publish(exchange='sensor_exchange',routing_key='sensorData',body=RabbitMessage)
+        print(RabbitMessage)
+        RabbitMessage=""
     else:
         print("Nothing has changed")
     
-
+#Method for converting node 300 on the CAN bus to readable data and sending it to the makeString method for assembling it into a combined string
 def id300(data):
     data = str(bytearray(data).hex())
     data1 = "0x" + data[4:6]
@@ -68,21 +65,18 @@ def id300(data):
     if (fuelLevel != int(data1, 0)):
         fuelLevel = int(data1, 0)
         makeString("fuelLevel:" + str(fuelLevel))
-
     data2 = "0x" + data[8:10]
     global hydraulicPressure
     if(hydraulicPressure!=int(data2, 0)):
         hydraulicPressure = int(data2, 0)
         makeString("hydraulicPressure:" + str(hydraulicPressure))
-
     data3 = "0x" + data[12:14]
     global hydraulicTemp
     if(hydraulicTemp!=int(data3,0)):
         hydraulicTemp = int(data3, 0)
         makeString("hydraulicTemp:" + str(hydraulicTemp))
 
-
-
+#Method for converting node 301 on the CAN bus to readable data and sending it to the makeString method for assembling it into a combined string
 def id301(data):
     data = str(bytearray(data).hex())
     data1 = "0x" + data[0:2]
@@ -93,37 +87,36 @@ def id301(data):
     data2 = "0x" + data[2:4]
     global motorRPM
     motorRPM = int(data2, 0)
-    # print('Motor RPM: ' + str(motorRPM))
 
-
+#Method for converting node 302 on the CAN bus to readable data and sending it to the makeString method for assembling it into a combined string
 def id302(data):
     data = str(bytearray(data).hex())
     data1 = "0x" + data[0:2]
     global TimeSinceHydServ
-    TimeSinceHydServ = int(data1, 0)
-    # print('Time since Hyd service: ' + str(TimeSinceHydServ))
+    if(TimeSinceHydServ!=int(data1,0)):
+        TimeSinceHydServ = int(data1, 0)
+        makeString("TimeSinceHydServ:" + TimeSinceHydServ)
     data2 = "0x" + data[4:6]
     global TimeSinceMotServ
-    TimeSinceMotServ = int(data1, 0)
-    # print('Time since Mot service: ' + str(TimeSinceMotServ))
+    if (TimeSinceMotServ != int(data2, 0)):
+        TimeSinceMotServ = int(data1, 0)
+        makeString("TimeSinceMotServ:" + TimeSinceMotServ)
 
-
+#Method for converting node 303 on the CAN bus to readable data and sending it to the makeString method for assembling it into a combined string
 def id303(data):
     data = str(bytearray(data).hex())
 
-
+#Method for converting node 304 on the CAN bus to readable data and sending it to the makeString method for assembling it into a combined string
 def id304(data):
     errorIndex = 0
     errorList = ''
     index = 0
     errors = [80, 40, 20, 10, 8, 4, 2, 1]
     alarmvalue = str(bytearray(data).hex())
-    # print(alarmvalue)
     col1 = alarmvalue[0:2]
     col2 = alarmvalue[2:4]
     col3 = alarmvalue[4:6]
     columns = [col1, col2, col3]
-    # print(col1, col2, col3)
     for value in columns:
         index = index + 1
         if (re.search('[a-zA-Z]', value)):
@@ -134,7 +127,6 @@ def id304(data):
             for error in errors:
                 if (error <= value):
                     value = value - error
-                    # print(errorIndex)
                     errorList = (errorList + str(errorIndex) + '-')
                 errorIndex = errorIndex + 1
     global alarms
@@ -144,45 +136,36 @@ def id304(data):
         makeString('alarms:' + str('"' + alarmsList + '"'))
 
 
-
+#This method is sorting the CAN messages into each arbitration Id
 def ReadCANData(col300, col301, col302, col303, col304):
-    messageID0 = col300
-    messageID1 = col301
-    messageID2 = col302
-    messageID3 = col303
-    messageID4 = col304
-    while messageID0 == False or messageID1 == False or messageID2 == False or messageID3 == False or messageID4 == False:
+    while col300 == False or col301 == False or col302 == False or col303 == False or col304 == False:
         message = bus.recv(10.0)  # timeout in seconds
 
-        if message.arbitration_id == 300 and messageID0 == False:
+        if message.arbitration_id == 300 and col300 == False:
             id300(message.data)
-            messageID0 = True
+            col300 = True
             
-        elif message.arbitration_id == 301 and messageID1 == False and messageID0==True:
+        elif message.arbitration_id == 301 and col301 == False and col300==True:
             id301(message.data)
-            messageID1 = True
+            col301 = True
 
-        elif message.arbitration_id == 302 and messageID2 == False and messageID1==True:
+        elif message.arbitration_id == 302 and col302 == False and col301==True:
             id302(message.data)
-            messageID2 = True
+            col302 = True
 
-        elif message.arbitration_id == 303 and messageID3 == False and messageID2==True:
+        elif message.arbitration_id == 303 and col303 == False and col302==True:
             id303(message.data)
-            messageID3 = True
+            col303 = True
 
-        elif message.arbitration_id == 304 and messageID4 == False and messageID3==True:
+        elif message.arbitration_id == 304 and col304 == False and col303==True:
             id304(message.data)
-            messageID4 = True
+            col304 = True
 
-        if (messageID4==True):
+        if (col304==True):
             SendString()
 
+#This keeps the script running infinitely
 while True:
-    col300 = False
-    col301 = False
-    col302 = False
-    col303 = False
-    col304 = False
-    ReadCANData(col300, col301, col302, col303, col304)
+    ReadCANData(False, False, False, False, False)
 
 connection.close()
