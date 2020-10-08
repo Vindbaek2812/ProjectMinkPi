93634e2e5cb1992e06a9c7b9e797caea74401879
Christian Vindbæk <christianvf99@gmail.com>
2020-10-08 10:45:36 +0200

    RPM should be working now

--- AzureTesting/CanTestFullList.py
+++ AzureTesting/CanTestFullList.py
@@ -6,18 +6,9 @@ while True:
     message = bus.recv(1.0)  # Timeout in seconds.
 
     if message.arbitration_id==301:
-        print(message)
-        #data = str(bytearray(message.data).hex())
-        #data1 = "0x" + data[4:6]
-        #value1 = int(data1, 0)
-        #print('RPMreader1: ' + str(value1))
-
         data = str(bytearray(message.data).hex())
-        data1 = "0x" + data[4:6]
-        data2 = "0x" + data[6:8]
-        value2 = int(data1, 0) + int(data2, 0)
-        print('RPMreader2: ' + str(value2))
+        data1 = data[4:6]
+        data2 = data[6:8]
+        value2 = int("0x" + data1, 0) + 256 * int("0x" + data2, 0)
+        print('RPMreader: ' + str(value2))
         print("=====================")
-        #1200 44 00 bc 04 00 00 00
-        #1400 43 00 88 05 00 00 00
-        #1600 
--- AzureTesting/RPMreader.py
+++ AzureTesting/RPMreader.py
@@ -1,7 +1,19 @@
-can="0000" + "3400" + "93020074"
+can1200="4400" + "bc04" + "000000"
+can1400="4300" + "8805" + "000000"
 
-print("0x" + can[4:6])
-data1 = "0x" + can[4:5]
-data2 = "0x" + can[5:6]
-value2 = int(data1, 0) + 256 * int(data2,0)
-print(value2)
+
+yes = "bc"
+no = "04"
+print(can1200[4:6])
+print(can1200[6:8])
+print(int("0x"+yes,0) +256* int("0x"+no,0))
+
+yes = "88"
+no = "05"
+#print(yes+no)
+print(int("0x"+yes,0) +256* int("0x"+no,0))
+#print("0x" + can1200[4:6])
+#data1 = "0x" + can1200[4:5]
+#data2 = "0x" + can1200[5:6]
+#value2 = int(data1, 0) + 256 * int(data2,0)
+#print(value2)
