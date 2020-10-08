820a935f76bde1b1c17c1c9dfd9d044097b0dd69
Vindbaek2812 <christianvf99@gmail.com>
2020-10-08 10:16:09 +0200

    reading values incorrectly

--- AzureTesting/CanTestFullList.py
+++ AzureTesting/CanTestFullList.py
@@ -7,14 +7,17 @@ while True:
 
     if message.arbitration_id==301:
         print(message)
-        data = str(bytearray(message.data).hex())
-        data1 = "0x" + data[4:6]
-        value1 = int(data1, 0)
-        print('RPMreader1: ' + str(value1))
+        #data = str(bytearray(message.data).hex())
+        #data1 = "0x" + data[4:6]
+        #value1 = int(data1, 0)
+        #print('RPMreader1: ' + str(value1))
 
         data = str(bytearray(message.data).hex())
-        data1 = "0x" + can[4:5]
-        data2 = "0x" + can[5:6]
-        value2 = int(data1, 0) + 256 * int(data2, 0)
+        data1 = "0x" + data[4:6]
+        data2 = "0x" + data[6:8]
+        value2 = int(data1, 0) + int(data2, 0)
         print('RPMreader2: ' + str(value2))
         print("=====================")
+        #1200 44 00 bc 04 00 00 00
+        #1400 43 00 88 05 00 00 00
+        #1600 
