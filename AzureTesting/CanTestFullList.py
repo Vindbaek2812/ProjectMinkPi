f476a1df5e93cd44167c289684bb6e3e48fff736
Christian Vindbæk <christianvf99@gmail.com>
2020-10-08 09:09:22 +0200

    Testing RPM

--- AzureTesting/CanTestFullList.py
+++ AzureTesting/CanTestFullList.py
@@ -4,25 +4,16 @@ can_interface = 'can0'
 bus = can.interface.Bus(can_interface, bustype='socketcan')
 while True:
     message = bus.recv(1.0)  # Timeout in seconds.
-    #print(message)
-    if message.arbitration_id==300:
-        #print(message)
-        data = str(bytearray(message.data).hex())
-        data1 = "0x" + data[4:6]
-        value1 = int(data1, 0)
-        #print('Fuel level: ' + str(value1))
-        data2 = "0x" + data[8:10]
-        value1 = int(data2, 0)
-        #print('Hydraulic Pressure: ' + str(value1))
-        
+
     if message.arbitration_id==301:
-        #print(message)
+        print(message)
         data = str(bytearray(message.data).hex())
         data1 = "0x" + data[4:6]
         value1 = int(data1, 0)
-        #print('RPM: ' + str(value1))
-    
-    if message.arbitration_id==304:
-        #print(message.data)
-        messageData = (str(bytearray(message.data).hex()))
-        print(messageData)
+        print('RPMreader1: ' + str(value1))
+
+        data = str(bytearray(message.data).hex())
+        data2 = "0x" + data[4:8]
+        value2 = int(data2, 0)
+        print('RPMreader2: ' + str(value2))
+        print("=====================")
--- Pi_Startup/consumer-to-azure.py
+++ Pi_Startup/consumer-to-azure.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 import pika
+import time
 from azure.iot.device import IoTHubDeviceClient, Message
 
 # Selects the IoT Hub based on user selection
 # Student IoT Hub
 CONNECTION_STRING = "HostName=ThyrrestrupMinkIoTHub.azure-devices.net;DeviceId=ThyrrestrupMinkDevice;SharedAccessKey=2veqtyJShmtnORMdBZTGUZtvX8Zm9i06y6YRz3kK+EU="
+client=""
 
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-
-channel.exchange_declare(exchange='sensor_exchange', exchange_type='topic')
+#connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+#channel = connection.channel()
+#channel.exchange_declare(exchange='sensor_exchange', exchange_type='topic')
 
 
 def iothub_client_init():
     # Create an IoT Hub client
     print("Connecting to " + "'"+CONNECTION_STRING+"'")
+    global client
     client = IoTHubDeviceClient.create_from_connection_string(
         CONNECTION_STRING)
     return client
-
 
 def callback(ch, method, properties, body):
     # print(str(body))
@@ -29,9 +29,11 @@ def callback(ch, method, properties, body):
     try:
         unsent = True
         while bool(unsent):
+            print(bool(client))
             if bool(client):
-                client.send_message(body)
-                print("Sending message: {}".format(body))
+
+                client.send_message(message)
+                print("Sending message: {}".format(message))
                 unsent = False
             else:
                 client = iothub_client_init()
@@ -40,8 +42,13 @@ def callback(ch, method, properties, body):
 
 
 queue_name = 'sensorData'
-channel.basic_consume(queue=queue_name,on_message_callback=callback,auto_ack=True)
+while True:
+    body="{" + '"' + "alarms" + '"' + ":" + '"' +"6-9-6-9" + '"' + "," + '"' + "nowTime" + '"' + ":" + '"' +"24/09/20 - 09:27:59" + '"' + "}"
+    callback(1,2,3, body)
+    time.sleep(5)
+
+#channel.basic_consume(queue=queue_name,on_message_callback=callback,auto_ack=True)
 
 #Printing the whole statement to troubleshoot
 print(' [*] PiTestConsumer is waiting for messages. To exit press CTRL + C...')
-channel.start_consuming()
+#channel.start_consuming()
